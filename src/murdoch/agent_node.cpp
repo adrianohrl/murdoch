34b4d5119bd36c5b109c83529cf012528f055207
adrianohrl <adrianohrl@unifei.edu.br>
2018-02-17 18:35:07 -0200

    fixed utility initiation

--- src/murdoch/agent_node.cpp
+++ src/murdoch/agent_node.cpp
@@ -3,6 +3,10 @@
 #include <talmech/auction/auctioneer_robot.h>
 #include <talmech/auction/bidder_agent.h>
 #include <talmech/auction/bidder_robot.h>
+#include <talmech/discrete_skill.h>
+#include <talmech/continuous_skill.h>
+#include <talmech/utility/basic/distance_utility.h>
+#include <talmech/utility/basic/skill_utility.h>
 
 namespace murdoch
 {
@@ -13,6 +17,7 @@ AgentNode::AgentNode(const ros::NodeHandlePtr& nh, const ros::Rate& rate)
 
 void AgentNode::readParameters()
 {
+  ROS_INFO("Reading parameters...");
   ros::NodeHandle pnh("~");
   std::string agent_type;
   pnh.param("type", agent_type, std::string("robot"));
@@ -29,10 +34,12 @@ void AgentNode::readParameters()
     if (role_type == "auctioneer")
     {
       agent_.reset(new talmech::auction::AuctioneerAgent(nh_, id));
+      ROS_INFO("   initializing agent as an AuctioneerAgent.");
     }
     else if (role_type == "bidder")
     {
       agent_.reset(new talmech::auction::BidderAgent(nh_, id));
+      ROS_INFO("   initializing agent as an BidderAgent.");
     }
     else
     {
@@ -44,10 +51,12 @@ void AgentNode::readParameters()
     if (role_type == "auctioneer")
     {
       agent_.reset(new talmech::auction::AuctioneerRobot(nh_, id));
+      ROS_INFO("   initializing agent as an AuctioneerRobot.");
     }
     else if (role_type == "bidder")
     {
       agent_.reset(new talmech::auction::BidderRobot(nh_, id));
+      ROS_INFO("   initializing agent as an BidderRobot.");
     }
     else
     {
@@ -57,6 +66,103 @@ void AgentNode::readParameters()
   else
   {
     throw utilities::Exception("Unknown agent type.");
+  }
+  if (role_type == "bidder")
+  {
+    std::string utility;
+    pnh.param("utility/expression", utility, std::string(""));
+    if (!utility.empty())
+    {
+      agent_->setUtility(utility);
+      ROS_INFO_STREAM("   ~/utility/expression: " << utility);
+    }
+  }
+  pnh = ros::NodeHandle("~/skills");
+  int size;
+  pnh.param("size", size, 0);
+  ROS_INFO_STREAM("   ~/skills/size: " << size);
+  for (int i(0); i < size; i++)
+  {
+    std::stringstream ss;
+    ss << "skill" << i << "/";
+    std::string resource_id;
+    pnh.param(ss.str() + "resource", resource_id, std::string(""));
+    ROS_INFO_STREAM("   ~/skills/skill" << i << "/resource: " << resource_id);
+    talmech::ResourcePtr resource(new talmech::Resource(resource_id));
+    int type;
+    pnh.param(ss.str() + "type", type, 0);
+    ROS_INFO_STREAM("   ~/skills/skill" << i << "/type: " << type);
+    talmech::SkillPtr skill;
+    if (type == 0)
+    {
+      skill.reset(new talmech::Skill(resource));
+    }
+    else if (type == 1)
+    {
+      int level;
+      pnh.param(ss.str() + "level", level, 0);
+      ROS_INFO_STREAM("   ~/skills/skill" << i << "/level: " << level);
+      skill.reset(new talmech::DiscreteSkill(resource, level));
+    }
+    else if (type == 2)
+    {
+      double level;
+      pnh.param(ss.str() + "level", level, 0.0);
+      ROS_INFO_STREAM("   ~/skills/skill" << i << "/level: " << level);
+      skill.reset(new talmech::ContinuousSkill(resource, level));
+    }
+    else
+    {
+      throw utilities::Exception("Invalid type of skill.");
+    }
+    agent_->addSkill(skill);
+  }
+  talmech::utility::UtilityComponentPtr component(
+      agent_->getUtilityComponent("SkillUtility"));
+  if (component)
+  {
+    talmech::utility::basic::SkillUtilityPtr skill_component(
+        boost::dynamic_pointer_cast<talmech::utility::basic::SkillUtility>(
+            component));
+    if (skill_component)
+    {
+      ROS_INFO("Initializing the SkillUtility component...");
+      std::list<double> correction_factors;
+      int size;
+      pnh.param("size", size, 0);
+      for (int i(0); i < size; i++)
+      {
+        std::stringstream ss;
+        ss << "skill" << i << "/";
+        double correction_factor;
+        pnh.param(ss.str() + "utility/skill", correction_factor, 1.0);
+        ROS_INFO_STREAM("   ~/skills/skill"
+                        << i << "/utility/skill: " << correction_factor);
+        correction_factors.push_back(correction_factor);
+      }
+      skill_component->init(*agent_, correction_factors);
+    }
+  }
+  if (agent_type == "robot")
+  {
+    component = agent_->getUtilityComponent("DistanceUtility");
+    if (component)
+    {
+      talmech::utility::basic::DistanceUtilityPtr distance_component(
+          boost::dynamic_pointer_cast<talmech::utility::basic::DistanceUtility>(
+              component));
+      if (distance_component)
+      {
+        pnh = ros::NodeHandle("~");
+        ROS_INFO("Initializing the DistanceUtility component...");
+        double correction_factor;
+        pnh.param("utility/distance", correction_factor, 1.0);
+        ROS_INFO_STREAM("   ~/utility/distance: " << correction_factor);
+        distance_component->init(
+            *boost::dynamic_pointer_cast<talmech::Robot>(agent_),
+            correction_factor);
+      }
+    }
   }
 }
 }
--- src/murdoch/task_generator_node.cpp
+++ src/murdoch/task_generator_node.cpp
@@ -16,54 +16,54 @@ void TaskGeneratorNode::readParameters()
   ros::NodeHandle pnh("~");
   double cycle_duration_mean, cycle_duration_std;
   pnh.param("cycle_duration/mean", cycle_duration_mean, 0.0);
-  ROS_INFO_STREAM("   ~cycle_duration/mean: " << cycle_duration_mean);
+  ROS_INFO_STREAM("   ~/cycle_duration/mean: " << cycle_duration_mean);
   pnh.param("cycle_duration/standard_deviation", cycle_duration_std, 0.0);
-  ROS_INFO_STREAM("   ~cycle_duration/standard_deviation: " << cycle_duration_std);
+  ROS_INFO_STREAM("   ~/cycle_duration/standard_deviation: " << cycle_duration_std);
   utilities::NoisyDurationPtr cycle_duration(
       new utilities::NoisyDuration(ros::Duration(cycle_duration_mean), cycle_duration_std));
   int waypoints_mean, waypoints_std;
   pnh = ros::NodeHandle("~/waypoints");
   pnh.param("mean", waypoints_mean, 0);
-  ROS_INFO_STREAM("   ~waypoints/mean: " << waypoints_mean);
+  ROS_INFO_STREAM("   ~/waypoints/mean: " << waypoints_mean);
   pnh.param("standard_deviation", waypoints_std, 1);
-  ROS_INFO_STREAM("   ~waypoints/standard_deviation: " << waypoints_std);
+  ROS_INFO_STREAM("   ~/waypoints/standard_deviation: " << waypoints_std);
   utilities::NoisyLongPtr waypoints(
         new utilities::NoisyLong(waypoints_mean, waypoints_std));
   double waypoints_x_mean, waypoints_x_std;
   pnh.param("x/mean", waypoints_x_mean, 0.0);
-  ROS_INFO_STREAM("   ~waypoints/x/mean: " << waypoints_x_mean);
+  ROS_INFO_STREAM("   ~/waypoints/x/mean: " << waypoints_x_mean);
   pnh.param("x/standard_deviation", waypoints_x_std, 1.0);
-  ROS_INFO_STREAM("   ~waypoints/x/standard_deviation: " << waypoints_x_std);
+  ROS_INFO_STREAM("   ~/waypoints/x/standard_deviation: " << waypoints_x_std);
   utilities::NoisyDoublePtr waypoints_x(
         new utilities::NoisyDouble(waypoints_x_mean, waypoints_x_std));
   double waypoints_y_mean, waypoints_y_std;
   pnh.param("y/mean", waypoints_y_mean, 0.0);
-  ROS_INFO_STREAM("   ~waypoints/y/mean: " << waypoints_y_mean);
+  ROS_INFO_STREAM("   ~/waypoints/y/mean: " << waypoints_y_mean);
   pnh.param("y/standard_deviation", waypoints_y_std, 1.0);
-  ROS_INFO_STREAM("   ~waypoints/y/standard_deviation: " << waypoints_y_std);
+  ROS_INFO_STREAM("   ~/waypoints/y/standard_deviation: " << waypoints_y_std);
   utilities::NoisyDoublePtr waypoints_y(
         new utilities::NoisyDouble(waypoints_y_mean, waypoints_y_std));
   pnh = ros::NodeHandle("~/skills");
   utilities::NoisySkillsPtr skills;
   int size;
   pnh.param("size", size, 0);
-  ROS_INFO_STREAM("   ~waypoints/size: " << size);
+  ROS_INFO_STREAM("   ~/skills/size: " << size);
   for (int i(0); i < size; i++)
   {
     std::stringstream ss;
     ss << "skill" << i << "/";
     std::string resource_id;
     pnh.param(ss.str() + "resource", resource_id, std::string(""));
-    ROS_INFO_STREAM("   ~skills/skill" << i << "/resource: " << resource_id);
+    ROS_INFO_STREAM("   ~/skills/skill" << i << "/resource: " << resource_id);
     talmech::ResourcePtr resource(new talmech::Resource(resource_id));
     int type;
     pnh.param(ss.str() + "type", type, 0);
-    ROS_INFO_STREAM("   ~skills/skill" << i << "/type: " << type);
+    ROS_INFO_STREAM("   ~/skills/skill" << i << "/type: " << type);
     double level_mean, level_std;
     pnh.param(ss.str() + "level/mean", level_mean, 0.0);
-    ROS_INFO_STREAM("   ~skills/skill" << i << "/level/mean: " << level_mean);
+    ROS_INFO_STREAM("   ~/skills/skill" << i << "/level/mean: " << level_mean);
     pnh.param(ss.str() + "level/standard_deviation", level_std, 1.0);
-    ROS_INFO_STREAM("   ~skills/skill" << i << "/level/standard_deviation: " << level_std);
+    ROS_INFO_STREAM("   ~/skills/skill" << i << "/level/standard_deviation: " << level_std);
     utilities::NoisyDoublePtr noisy_level(
           new utilities::NoisyDouble(level_mean, level_std));
     talmech::SkillPtr skill;
@@ -74,13 +74,13 @@ void TaskGeneratorNode::readParameters()
     else if (type == 1)
     {
       long level(round(noisy_level->random()));
-      ROS_INFO_STREAM("   ~skills/skill" << i << "/level: " << level);
+      ROS_INFO_STREAM("   ~/skills/skill" << i << "/level: " << level);
       skill.reset(new talmech::DiscreteSkill(resource, level));
     }
     else if (type == 2)
     {
       double level(noisy_level->random());
-      ROS_INFO_STREAM("   ~skills/skill" << i << "/level: " << level);
+      ROS_INFO_STREAM("   ~/skills/skill" << i << "/level: " << level);
       skill.reset(new talmech::ContinuousSkill(resource, level));
     }
     else
